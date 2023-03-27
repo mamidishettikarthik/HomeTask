d603c75bac1a2cb21d767c60f2d58d49a13db16a
mamidishettikarthik <124386275+mamidishettikarthik@users.noreply.github.com>
2023-03-27 07:41:56 +0530

    Unit Testing

---
+++ unit_testing/jasmine-standalone-4.6.0/spec/spec.js
@@ -0,0 +1,38 @@
+describe("Youtube Api Testing", function () {
+  
+    it("API result length is as expected", function () {
+      return getResponse("Epam").then(function (result) {
+        expect(result.length).toEqual(50);
+      });
+    });
+  
+    it("toNumPage is working as expected", () => {
+      const res = toNumPage(100);
+      expect(res).toEqual(10);
+    });
+  
+    it("nextPage is Working as expected condition-1", () => {
+      const res = nextPage(5);
+      expect(res).toBe(6);
+    });
+  
+    it("prevPage is working as expected condition-1", () => {
+      const res = prevPage(25);
+      expect(res).toBe(24);
+    });
+  
+    it("prevPage is working as expected condition-2", () => {
+      const res = prevPage(-1);
+      expect(res).toEqual(jasmine.any(Error));
+    });
+  
+    it("prevPage is working as expected condition-3", () => {
+      const res = prevPage(0);
+      expect(res).toEqual(jasmine.any(Error));
+    });
+    it("nextPage is Working as expected condition-2", () => {
+      const res = nextPage(20);
+      expect(res).toEqual(jasmine.any(Error));
+    });
+  
+  });
---
+++ unit_testing/jasmine-standalone-4.6.0/src/index.js
@@ -0,0 +1,37 @@
+var obj = [];
+var objPerPage = 10;
+async function getResponse(searchQuery) {
+    const apiKey = "AIzaSyDJijpMHa4XIPhhUPikeKv7iAuB0EB8USA";
+    const response = await fetch(
+        `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&type=video&part=snippet&maxResults=50&q=${searchQuery}`
+    );
+    if (!response.ok) {
+        throw new Error(`HTTP ERROR status: ${response.status}`);
+    }
+    const data = await response.json();
+    obj = data.items;
+    return obj;
+}
+function toNumPage(objLength) {
+    return Math.ceil(objLength / objPerPage);
+}
+function prevPage(currentPage) {
+    if (currentPage > 1) {
+        currentPage--;
+        return currentPage;
+    }
+    else {
+        const error = new Error("Expected a positive value");
+        return error;
+    }
+}
+function nextPage(currentPage) {
+    if (currentPage < toNumPage(100)) {
+        currentPage++;
+        return currentPage;
+    }
+    else {
+        const error = new Error("CurrentPage is value is greater than toNumPage value");
+        return error;
+    }
+}
