6c2ac933290632ed9aa7d54226000d4fe90a488f
mamidishettikarthik <124386275+mamidishettikarthik@users.noreply.github.com>
2023-03-29 11:01:56 +0530

    Webpack

---
+++ Webpack/src/index.js
@@ -0,0 +1,4 @@
+// import './styles/main.scss'
+function myFunction() {
+    document.getElementById("demo").innerHTML = "webpack and babe Babel is a free and open-source JavaScript transcompiler that is mainly used to convert ECMAScript 2015+ code into backwards-compatible JavaScript code that can be run by older JavaScript engines. It allows web developers to take advantage of the newest features of the language";
+  }
