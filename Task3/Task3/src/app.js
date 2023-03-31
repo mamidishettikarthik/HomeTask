a6953a40f2da7a1d19555c91c2c1a4d2a2686296
mamidishettikarthik <124386275+mamidishettikarthik@users.noreply.github.com>
2023-03-31 18:32:09 +0530

    Update app.js

--- Task3/Task3/src/app.js
+++ Task3/Task3/src/app.js
@@ -9,11 +9,6 @@ app.use(express.json());
 
 const PORT = 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
