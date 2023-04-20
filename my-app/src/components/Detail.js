cbea265d24940a2928cf0f2eeb2191351cbd6ebb
mamidishettikarthik <124386275+mamidishettikarthik@users.noreply.github.com>
2023-04-20 08:47:13 +0530

    Update Detail.js

--- my-app/src/components/Detail.js
+++ my-app/src/components/Detail.js
@@ -4,14 +4,14 @@ function Detail({ selected, closeDetail }) {
 return (
 	<section className="detail">
 	<div className="content">
-		<h2>{selected.Title}</h2>
-		<span>{selected.Year}</span>
+		<h2>{selected.title}</h2>
+		<span>{selected.year}</span>
 		<p className="rating">Rating: {selected.imdbRating}</p>
 
 		<div className="about">
 		<img src={selected.Poster} alt="" />
 			
-<p>{selected.Plot}</p>
+                <p>{selected.Plot}</p>
 
 		</div>
 		<button className="close" onClick={closeDetail}>
