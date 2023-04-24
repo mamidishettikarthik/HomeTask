ed914284c83a467028e7ce7ec041847e93aa8463
mamidishettikarthik <124386275+mamidishettikarthik@users.noreply.github.com>
2023-04-24 09:37:13 +0530

    Update Result.js

--- my-app/src/components/Result.js
+++ my-app/src/components/Result.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 function Result({ result, openDetail }) {
-return (
-	<div className="result" onClick=
-		{() => openDetail(result.imdbID)}>
-	<img src={result.Poster} />
-	<h3>{result.Title}</h3>
-	</div>
-);
+	return (
+		<div className="result" onClick=
+			{() => openDetail(result.imdbID)}>
+			<img src={result.Poster}  alt="poster result"/>
+			<h3>{result.title}</h3>
+		</div>
+	);
 }
 
 export default Result;
