523802d7d5259d6395418c42cae0fefccb998beb
mamidishettikarthik <124386275+mamidishettikarthik@users.noreply.github.com>
2023-04-24 12:51:40 +0530

    Update Detail.js

--- my-app/src/components/Detail.js
+++ my-app/src/components/Detail.js
@@ -1,25 +1,23 @@
 import React from "react";
 
 function Detail({ selected, closeDetail }) {
-return (
-	<section className="detail">
-	<div className="content">
-		<h2>{selected.title}</h2>
-		<span>{selected.year}</span>
-		<p className="rating">Rating: {selected.imdbRating}</p>
+	return (
+		<section className="detail">
+			<div className="content">
+				<h2>{selected.title}</h2>
+				<span>{selected.year}</span>
+				<p className="rating">Rating: {selected.imdbRating}</p>
 
-		<div className="about">
-		<img src={selected.Poster} alt="" />
-			
-                <p>{selected.Plot}</p>
-
-		</div>
-		<button className="close" onClick={closeDetail}>
-		Close
-		</button>
-	</div>
-	</section>
-);
+				<div className="about">
+					<img src={selected.poster} alt="" />
+					<p>{selected.plot}</p>
+				</div>
+				<button className="close" onClick={closeDetail}>
+					Close
+				</button>
+			</div>
+		</section>
+	);
 }
 
 export default Detail;
