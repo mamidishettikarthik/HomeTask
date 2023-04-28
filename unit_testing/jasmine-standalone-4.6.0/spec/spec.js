74f085b33f48da76019774fc6e4b4b42e995924f
mamidishettikarthik <124386275+mamidishettikarthik@users.noreply.github.com>
2023-04-28 12:50:54 +0530

    Update spec.js

--- unit_testing/jasmine-standalone-4.6.0/spec/spec.js
+++ unit_testing/jasmine-standalone-4.6.0/spec/spec.js
@@ -1,38 +1,38 @@
 describe("Youtube Api Testing", function () {
-  
-    it("API result length is as expected", function () {
-      return getResponse("Equal").then(function (result) {
-        expect(result.length).toEqual(50);
-      });
+
+  it("API result length is as expected", function () {
+    return getResponse("Epam").then(function (result) {
+      expect(result.length).toEqual(50);
     });
-  
-    it("toNumPage is working as expected", () => {
-      const res = toNumPage(100);
-      expect(res).toEqual(10);
-    });
-  
-    it("nextPage is Working as expected condition-1", () => {
-      const res = nextPage(5);
-      expect(res).toBe(6);
-    });
-  
-    it("prevPage is working as expected condition-1", () => {
-      const res = prevPage(25);
-      expect(res).toBe(24);
-    });
-  
-    it("prevPage is working as expected condition-2", () => {
-      const res = prevPage(-1);
-      expect(res).toEqual(jasmine.any(Error));
-    });
-  
-    it("prevPage is working as expected condition-3", () => {
-      const res = prevPage(0);
-      expect(res).toEqual(jasmine.any(Error));
-    });
-    it("nextPage is Working as expected condition-2", () => {
-      const res = nextPage(20);
-      expect(res).toEqual(jasmine.any(Error));
-    });
-  
   });
+
+  it("toNumPage is working as expected", () => {
+    const res = toNumPage(100);
+    expect(res).toEqual(10);
+  });
+
+  it("nextPage is Working as expected ", () => {
+    const res = nextPage(5);
+    expect(res).toBe(6);
+  });
+
+  it("prevPage is working as expected ", () => {
+    const res = prevPage(25);
+    expect(res).toBe(24);
+  });
+
+  it("expected a positive value is working", () => {
+    const res = prevPage(-1);
+    expect(res).toEqual(jasmine.any(Error));
+  });
+
+  it("positive value which is greater than 0 is working as expected ", () => {
+    const res = prevPage(0);
+    expect(res).toEqual(jasmine.any(Error));
+  });
+  it("CurrentPage is value is greater than toNumPage value is working as expected ", () => {
+    const res = nextPage(20);
+    expect(res).toEqual(jasmine.any(Error));
+  });
+
+});
