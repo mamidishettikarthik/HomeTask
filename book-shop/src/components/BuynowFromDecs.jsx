0307e3bbc8ffebca601e9d8a768be995a709388f
mamidishettikarthik <124386275+mamidishettikarthik@users.noreply.github.com>
2023-06-30 07:26:18 +0530

    Add files via upload

---
+++ book-shop/src/components/BuynowFromDecs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const Books = (props) => {
+    return (
+        <NavLink to={`/buynow/${props.value.id}`} class="btn btn-outline-primary">Buy Now</NavLink>
+    )
+}
+
+export default Books;
---
+++ book-shop/src/components/buttons/MyOrdersBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+const MyOrdersBtn = () => {
+    return (
+        <>
+            <NavLink to="/myorders" className="btn btn-outline-primary ms-2">
+                <span className="fa fa-history"></span> My Orders
+            </NavLink>
+        </>
+    )
+}
+export default MyOrdersBtn;
